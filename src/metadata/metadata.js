799b12c7235e175554349ae60c91aa177bffb7de
nikolas-virionis <nikolas.virionis@bandtec.com.br>
2023-06-12 11:21:38 -0300

    v1 nikolas CV

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,68 +1,119 @@
 module.exports = {
-  name: 'William Shakespeare',
-  title: 'Poet, playwright and actor',
+  name: 'Nikolas B Virionis',
+  title: 'Python Developer and Data Engineer',
   facts: {
-    'Residence': '<a href="https://www.google.com/maps/place/London,+UK/@51.5285582,-0.2416814,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862"><i class="fa fa-home fact-icon"></i>London</a>',
-    'Wiki': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-                  <i class="fa-brands fa-wikipedia-w fact-icon"></i>William_Shakespeare
+    'Residence': '<a href="https://www.google.com/maps/place/R.+Tutóia,+701-900+-+Vila+Mariana,+São+Paulo+-+SP,+04007-004/@-23.578072,-46.6530841,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce5994b134d16b:0xf60aafdf8f1f0a55!8m2!3d-23.5780592!4d-46.6508468!16s%2Fg%2F1v4pv3q8?entry=ttu"><i class="fa fa-home fact-icon"></i>São Paulo, SP, Brazil</a>',
+    'Linkedin': `<a href="https://www.linkedin.com/in/nikolas-barcha-virionis/">
+                  <i class="fa-brands fa-linkedin fact-icon"></i>Nikolas B Virionis
                 </a>`,
-    'Twitter': `<a href="https://twitter.com/shakespeare"><i class="fa-brands fa-twitter fact-icon"></i>Shakespeare</a>`,
-    'Email': '<a href="mailto:wil64@yahoo.com"><i class="fa fa-envelope fact-icon"></i>wil64@yahoo.com</a>'
+    'Github': `<a href="https://github.com/nikolas-virionis"><i class="fa-brands fa-github fact-icon"></i>nikolas-virionis</a>`,
+    'Email': '<a href="mailto:nikolas.virionis@gmail.com"><i class="fa fa-envelope fact-icon"></i>nikolas.virionis@gmail.com</a>'
   },
   skills: [
-    ['Comedy', 100],
-    ['Love', 100],
-    ['Theater', 100],
-    ['Tragedy', 100],
-    ['Poetry', 100],
-    ['Erotic', 75],
-    ['Sonnets', 100],
-    ['Plays', 100],
-    ['Writing', 100],
-    ['Directing', 75],
-    ['Literature', 100],
-    ['Influence', 100],
+    ['Python', 90],
+    ['PySpark', 80],
+    ['English', 80],
+    ['AWS', 75],
+    ['SQL', 75],
+    ['Pandas', 70],
+    ['Infrastructure as Code', 70],
+    ['IaC (AWS CDK)', 70]
   ],
   positions: [
     {
-      title: 'Hamlet',
-      period: 'Likely early 17th century',
-      skills: ['tragedy', 'philosophy', 'death'],
-      contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
-`
-    },
-    {
-      title: 'Twelfth Night',
-      period: '1600 – 1601',
-      skills: ['comedy', 'love'],
-      contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
-    },
-    {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
+      title: 'Data Engineering Intern at yHub',
+      period: 'Jan/2022 ~ Present',
+      skills: ['Python', 'PySpark', 'AWS', 'SQL', 'Infrastructure as Code', 'AWS CDK'],
+      contents: `As a Data Engineering Intern at yHub, I gained invaluable hands-on experience in building cost-effective data pipelines using mostly serverless AWS Services. Collaborating closely with the team, I actively contributed to end-to-end development, problem-solving, and maintenance of the projects. Leveraging my skills in Python, Pandas, and PySpark, I implemented best practices to ensure efficient and scalable data processes. I proactively sought opportunities for improvement and successfully achieved three AWS Certifications in a few months of my professional career, allowing me to actively participate in solution discussions and make humble contributions to the team's success. Through my involvement in critical projects, I supported data-driven decision-making and enhanced my technical expertise while fostering effective collaboration within cross-functional teams.`
     }
+  ],
+  educations: [
+    {
+      title: 'Bachelor of Computer Science at São Paulo Tech School',
+      period: 'Jan/2021 ~ Dec/2024',
+      skills: ['Python', 'PySpark', 'AWS', 'SQL', 'Javascript', 'Java', 'Microsoft Azure', 'Data Structures and Algorithms'],
+      contents: `Bachelor's degree in Computer Science with a strong emphasis on practical, real-world technologies and problem-solving skills. Gained valuable soft skills through dedicated "socio-emotional development" classes and acquired significant experience from engaging in extensive hands-on projects focused on real-world applications.`
+    },
+    {
+        title: 'High School at Colégio Dante Alighieri',
+        period: 'Jan/2018 ~ Dec/2020',
+        skills: ['Python', 'Problem Solving'],
+        contents: `I graduated as one of the top students in my class from one of São Paulo's most prestigious schools. During my academic journey, I had the valuable opportunity to kickstart my tech career by actively participating in elective classes focused on Python programming and problem solving.`
+    }
+  ],
+  certifications: [
+    {
+      title: 'AWS Certified Cloud Practitioner',
+      period: 'Apr/2022',
+      url: 'https://www.credly.com/badges/c0ffb338-5236-458a-a692-0441827ec7e3/linked_in_profile',
+    },
+    {
+      title: 'AWS Certified Developer Associate',
+      period: 'Aug/2022',
+      url: 'https://www.credly.com/badges/85e57612-1339-4013-89a7-390d8454cfed/linked_in_profile',
+    },
+    {
+      title: 'AWS Certified Solutions Architect Associate',
+      period: 'Nov/2022',
+      url: 'https://www.credly.com/badges/147e5d6e-3bf3-4de3-87e6-296d8006d2a4/linked_in_profile',
+    },
+  ],
+  courses: [
+    {
+      title: 'Taming Big Data with Apache Spark and Python, on Udemy',
+      period: 'Jan/2022',
+      skills: ['Python', 'PySpark'],
+      url: 'https://www.udemy.com/certificate/UC-7046784c-c39f-422d-8b31-ce783ce7cd41/'
+    },
+    {
+      title: 'Machine Learning, Data Science and Deep Learning with Python, on Udemy',
+      period: 'Jan/2022',
+      skills: ['Python', 'PySpark', 'Machine Learning', 'Pandas'],
+      url: 'https://www.udemy.com/certificate/UC-548dbf2e-1d88-4356-8c8a-9c7fb204e4cc/'
+    },
+    {
+      title: 'Cloud Computing 101, on AWS Educate',
+      period: 'Jan/2022',
+      skills: ['AWS'],
+      url: 'https://www.credly.com/badges/b378e9b5-337d-4878-a72f-f1ae300103a7/public_url'
+    },
+    {
+      title: 'Scala and Spark for Big Data and Machine Learning',
+      period: 'Jan/2023',
+      skills: ['Scala', 'Spark'],
+      url: 'https://www.udemy.com/certificate/UC-62bb400f-5e92-47e6-87f7-e92eb682e778/'
+    },
   ],
   experience: [
     {
-      header: 'Performance',
-      skills: ['theater', 'play'],
-      contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
+      header: 'NPM published package',
+      skills: ['Machine Learning',  'Javascript'],
+      contents: `The linear-regression-model package provides a simpler way to get The linear regression and correlation between two variables in Node.`,
+      url: 'https://github.com/nikolas-virionis/linear-regression-model'
     },
     {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
-      contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. I dedicated them to Henry Wriothesley, Earl of Southampton.
-`
-    }
+      header: 'PyPI published package #1',
+      skills: ['Python', 'Machine Learning'],
+      contents: `The polynomial-regression-model package aims at from the data the user gives it, it deduces the best regression model to analyse it and returns the model, fit for predictions.`,
+      url: 'https://github.com/nikolas-virionis/polynomial-regression'
+    },
+    {
+      header: 'PyPI published package #2',
+      skills: ['Python', 'Machine Learning', 'Data Analysis', 'Working with external APIs'],
+      contents: `The spotify-recommender-api package serves the purpose of providing both recommendations for the user and a way for one to "cluster" automatically songs within a big playlist, base on the genres, popularity and song features, for example.`,
+      url: 'https://github.com/nikolas-virionis/spotify-api'
+    },
+    {
+      header: 'College project #1 - Safelog',
+      skills: ['Node', 'Java', 'AWS EC2'],
+      contents: `(aug/2021 - dec/2021) A Freshman year, group project in college, that collects machine health data and sets up alarms and dashboards based on that information.`,
+      url: 'https://github.com/nikolas-virionis/Safelog'
+    },
+    {
+      header: 'College project #2 - Homebox',
+      skills: ['React', 'Java Spring Boot', 'AWS'],
+      contents: `(jan/2022 - dec/2022) A group project in college, that provides an peer to peer business model platform (uber-like) around home services, such as plumbing or painting.`,
+      url: 'https://github.com/nikolas-virionis'
+    },
   ]
 };
